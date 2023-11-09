0ae77886e8d2eb501c046f64227180c2623295d5
Basith P <basithpofficial@gmail.com>
2023-11-09 22:09:22 +0530

    feat: update feed post ui

--- lib/core/common/views/main_layout.dart
+++ lib/core/common/views/main_layout.dart
@@ -8,7 +8,10 @@ class MainLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Altlink'),
+        title: const Text(
+          'Altlink',
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.logout_rounded))
         ],
